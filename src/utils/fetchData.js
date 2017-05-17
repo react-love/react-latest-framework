222706dbe4a2ba6b0759163e34d7e74dda60e086
yongyuehuang <1130216245@qq.com>
2017-05-17 11:59:50 +0800

    修改axios接口

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,25 +1,13 @@
 /**
  * Created by admin on 2016/10/10.
  */
-import axios from 'axios'
+import axios from 'axios';
 
 //封装好的get和post接口，调用方法情况action文件
-export let instance = axios.create({
+const instance = axios.create({
     // baseURL: API_URL, //设置默认api路径
-    timeout: 10000, //设置超时时间
+    timeout: 2, //设置超时时间
     headers: {'X-Custom-Header': 'foobar'}
 });
 
-export const getData = (url, param) => {
-    return (
-        instance.get(`${url}`, {
-            params: param
-        })
-    )
-}
-
-export const postData = (url, param) => {
-    return (
-        instance.post(`${url}`, param)
-    )
-}
+export default instance;
