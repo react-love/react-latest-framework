587382c5854b922abee86160cb27c33fe81bbd8c
yongyuehuang <1130216245@qq.com>
2017-08-26 23:35:18 +0800

    优化Special组件

--- src/components/Home/Special.js
+++ src/components/Home/Special.js
@@ -1,27 +1,24 @@
 /**
  * Created by Administrator on 2016/7/2.
  */
-import React from 'react';
+import React from 'react'
 
 /*files*/
-const zht_1 = require('./files/zht1.jpg');
-const zht_2 = require('./files/zht2.jpg');
+const zht_1 = require('./files/zht1.jpg')
+const zht_2 = require('./files/zht2.jpg')
 
-export default class Special extends React.Component {
-
-    render() {
-
-        return (
-            <div className="style_main">
-                <div className="style_div">
-                    <div className="style_left">
-                        <img src={zht_1} className="style_img" />
-                    </div>
-                    <div className="style_right">
-                        <img src={zht_2} className="style_img" />
-                    </div>
+const Special = () => {
+    return (
+        <div className="style_main">
+            <div className="style_div">
+                <div className="style_left">
+                    <img src={zht_1} className="style_img" />
+                </div>
+                <div className="style_right">
+                    <img src={zht_2} className="style_img" />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+export default Special
