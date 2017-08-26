ca654162ce6563eb31b20e3bcdc65509d4cced64
yongyuehuang <1130216245@qq.com>
2017-08-27 00:54:23 +0800

    优化入口文件

--- src/entry.js
+++ src/entry.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
-import { AppContainer } from 'react-hot-loader';
-import App from './App';
-import createHistory from 'history/createBrowserHistory';
-import rootReducer from './reducers/index';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { AppContainer } from 'react-hot-loader'
+import App from './App'
+import createHistory from 'history/createBrowserHistory'
+import rootReducer from './reducers/index'
 
-var FastClick = require('fastclick');
+var FastClick = require('fastclick')
 
 //按模块导入lodash，可以有效减小vendor.js的大小
-import isEmpty from 'lodash/isEmpty';
-import isEqual from 'lodash/isEqual';
-import debounce from 'lodash/debounce';
-import isArray from 'lodash/isArray';
+import isEmpty from 'lodash/isEmpty'
+import isEqual from 'lodash/isEqual'
+import debounce from 'lodash/debounce'
+import isArray from 'lodash/isArray'
 
-window.isEmpty = isEmpty;
-window.isEqual = isEqual;
-window.debounce = debounce;
-window.isArray = isArray;
+window.isEmpty = isEmpty
+window.isEqual = isEqual
+window.debounce = debounce
+window.isArray = isArray
 
-const history = createHistory();
+const history = createHistory()
 const middleware = routerMiddleware(history)
 
 //解决移动端300毫秒延迟
-FastClick.attach(document.body);
-const middlewares = [thunk, middleware];
+FastClick.attach(document.body)
+const middlewares = [thunk, middleware]
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(
+    combineReducers({routing: routerReducer, ...rootReducer}),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
 
 const render = Component =>
     ReactDOM.render(
@@ -40,7 +43,7 @@ const render = Component =>
             </Provider>
         </AppContainer>,
         document.getElementById('root')
-    );
+    )
 
 render(App)
 
@@ -48,5 +51,5 @@ if(module.hot) {
     module.hot.accept('./App', () => {
         const NextRootContainer = require('./App').default
         render(NextRootContainer)
-    });
+    })
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,10 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux'
-
-import { home } from './home';
-import { search } from './search';
-import { global } from './global';
-
-//注册reducer，每个自定义的reducer都要来这里注册！！！不注册会报错。
-const rootReducer = combineReducers({
-  routing: routerReducer,
+import { home } from './home'
+import { search } from './search'
+import { global } from './global'
+const rootReducer = {
   /* your reducers */
   home, //首页相关
   search, //搜索相关
   global
-});
-
-export default rootReducer;
+}
+export default rootReducer
