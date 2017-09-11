1e655268385409e735f526bf8c6868cc54b7a3da
yongyuehuang <1130216245@qq.com>
2017-09-11 23:16:42 +0800

    app.js路径修改

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import asyncComponent from './AsyncComponent'
 import Home from 'containers/Home/Home'
 import ReactChildrenMap from 'utils/ReactChildrenMap'
 const Search = asyncComponent(() => import(/* webpackChunkName: "search" */ "./containers/Search/Search"))
-const BookList = asyncComponent(() => import(/* webpackChunkName: "bookList" */ "./containers/BookList/BookListContainer"))
+const BookList = asyncComponent(() => import(/* webpackChunkName: "bookList" */ "./containers/BookList/BookList"))
 
 @connect (
     state => {return {...state.global}},
